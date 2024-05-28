3fe5e744e6f722e03507fa4701773287eda0fb10
Al katef <alkatef@Als-Mac-mini.local>
2024-05-28 11:24:01 +0530

    updates

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/header.css";
-import printer from "../assets/printers.png";
+import printer0 from "../assets/printers.png";
+import printer1 from "../assets/printers2.png";
 import printer2 from "../assets/konica22.png";
 import printer3 from "../assets/hp.webp";
 import dc from "../assets/dc.png";
@@ -11,25 +12,28 @@ const Header = () => {
     <>
       <div className="gray-bg">
         <div className="printer-img child1">
-          <img src={dc} alt="" />
+          <img className="logo" src={dc} alt="" />
 
           <div className="bg-printer">
-            <img className="printer-img" src={printer} alt="" />
+            <img className="printer-image img1" src={printer0} alt="" />
+            <img className="printer-image img2" src={printer1} alt="" />
           </div>
-          <h3 className="offer-price">Konica printer AED 100</h3>
+          <h3 className="offer-price">Monthly expense <br /> Konica printer AED 100</h3>
+          <h3 className="offer-price price2">Monthly expense <br /> Inkjet printer AED 250</h3>
+
         </div>
 
         <div className="printer-content child2">
           <img className="offer-gif" src={gif} alt="" />
           <div className="offer-flex">
             <h1>
-              <span className="weight">Save 75% On</span>
+              <span className="weight">Save 60% On</span>
               <br />
               Total Monthly Cost
             </h1>
           </div>
           <h2 className="offer-details">
-            Are you currently shelling out AED 200 for toners, you're aiming to
+            Are you currently shelling out AED 250 for toners, you're aiming to
             slash that expense in half down to AED 100.
           </h2>
           <p>
@@ -47,7 +51,7 @@ const Header = () => {
           <div className="bg-printer">
             <img className="printer-img" src={printer3} alt="" />
           </div>
-          <h3 className="offer-price">Inkjet printer AED 250</h3>
+          <h3 className="offer-price">Monthly expense <br /> Inkjet printer AED 250</h3>
         </div>
       </div>
     </>
